b9cd017447832491b65869298674ed783afc8d18
小桂花 <143058334+guihuala@users.noreply.github.com>
2025-05-25 16:17:37 +0800

    导入一个音频框架

--- Assets/Scripts/UIFrameworkExamples/ScreenControllers/SettingWindowController.cs
+++ Assets/Scripts/UIFrameworkExamples/ScreenControllers/SettingWindowController.cs
@@ -10,40 +10,42 @@ using Utils;
 public class SettingWindowController : WindowController
 {
     private bool isSaved = false;
-    protected override void Awake() {
+
+    protected override void Awake()
+    {
         base.Awake();
         isSaved = false;
     }
+
     public void UI_Save()
     {
         isSaved = true;
-        
+
         //TODO
         //这里放保存设置的方法
-        
     }
-    public void UI_ShowPopup() {
-        if(!isSaved)
+
+    public void UI_ShowPopup()
+    {
+        if (!isSaved)
             Signals.Get<ShowConfirmationPopupSignal>().Dispatch(GetPopupData());
         else
             UI_Close();
     }
+
     private ConfirmationPopupProperties GetPopupData()
     {
         ConfirmationPopupProperties testProps = null;
-        testProps = new ConfirmationPopupProperties("确认设置", 
+        testProps = new ConfirmationPopupProperties("确认设置",
             "现在退出设置会失效，确认要退出吗",
-            "确认",UI_Close,
-            "取消",DoNothing
-            );
-        
+            "确认", UI_Close,
+            "取消", DoNothing
+        );
+
         return testProps;
-        
     }
 
     private void DoNothing()
     {
-        
     }
-    
 }
