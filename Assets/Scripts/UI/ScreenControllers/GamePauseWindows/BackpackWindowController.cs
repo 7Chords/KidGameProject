bed741c7efc9c6400d42cf42c8a33fe81f9c1121
Slime862 <2058456435@qq.com>
2025-06-28 21:07:15 +0800

    dev:物品描述

--- Assets/Scripts/UI/ScreenControllers/GamePauseWindows/BackpackWindowController.cs
+++ Assets/Scripts/UI/ScreenControllers/GamePauseWindows/BackpackWindowController.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using KidGame.Core;
 using KidGame.UI;
+using KidGame.UI.Game;
+using TMPro;
 using UnityEngine;
+using Utils;
 
 public class BackpackWindowController : WindowController
 {
     private UICircularScrollView scrollView;
     private List<MaterialSlotInfo> _materialSlotInfos;
     private List<TrapSlotInfo> _trapSlotInfos;
+    private GameObject detailPanel;
+    private TextMeshProUGUI detailText;
+    
     protected override void Awake()
     {
         base.Awake();
-        
+        detailPanel = transform.Find("BackPag/ScrollView/DetailPanel").gameObject;
+        detailText = transform.Find("BackPag/ScrollView/DetailPanel/DetailText").GetComponent<TextMeshProUGUI>();
+        detailPanel.SetActive(false);
+        Signals.Get<ShowItemDetailSignal>().AddListener(OnShowItemDetail);
+        Signals.Get<HideItemDetailSignal>().AddListener(OnHideItemDetail);
     }
 
     protected override void OnPropertiesSet() {
@@ -21,6 +31,13 @@ public class BackpackWindowController : WindowController
         _trapSlotInfos = PlayerBag.Instance.GetTrapSlots();
         scrollView.Init(_materialSlotInfos.Count + _trapSlotInfos.Count,OnCellUpdate,null);
         
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        Signals.Get<ShowItemDetailSignal>().RemoveListener(OnShowItemDetail);
+        Signals.Get<HideItemDetailSignal>().RemoveListener(OnHideItemDetail);
     }
 
     private void OnCellUpdate(GameObject cell,int index)
@@ -33,5 +50,30 @@ public class BackpackWindowController : WindowController
         {
             cellUI.SetUIWithTrap(_trapSlotInfos[index - 1 - _materialSlotInfos.Count]);
         }
+    }
+
+    private void OnShowItemDetail(CellUI cellUI)
+    {
+        /*// 获取鼠标在屏幕上的位置
+        Vector2 mousePosition = Input.mousePosition;
+        // 将鼠标屏幕坐标转换为 Canvas 的局部坐标
+        RectTransform canvasRectTransform = UIController.Instance.uiCanvas.transform as RectTransform;
+        Vector2 localMousePosition;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, mousePosition, null, out localMousePosition);
+        // 应用偏移量（往右下一点）
+        Vector2 offset = new Vector2(10, -10); // 偏移量，可以根据需要调整
+        localMousePosition += offset;
+
+        // 设置详细描述面板的位置
+        detailPanel.GetComponent<RectTransform>().anchoredPosition = localMousePosition;*/
+        
+        detailPanel.transform.position = cellUI.transform.position + new Vector3(1.5f, -1.5f, 0f);
+        detailText.text = cellUI.detailText;
+        detailPanel.SetActive(true);
+    }
+
+    private void OnHideItemDetail()
+    {
+        detailPanel.SetActive(false);
     }
 }
--- Assets/Scripts/UI/Widgets/CellUI.cs
+++ Assets/Scripts/UI/Widgets/CellUI.cs
@@ -2,29 +2,108 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using KidGame.Core;
+using KidGame.UI.Game;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Utils;
+
+
+public class ShowItemDetailSignal : ASignal<CellUI>
+{
+}
+public class HideItemDetailSignal : ASignal
+{
+}
 
 public class CellUI : MonoBehaviour
 {
+    private EventTrigger eventTrigger;
+    
     public Image icon;
     public TextMeshProUGUI count;
+    
+    
+    public string detailText;
+
+    private float timer = 0f;
+    private const float delay = 0.6f; // 悬停时间阈值
     private void Awake()
     {
+        eventTrigger = GetComponent<EventTrigger>();
+        
         icon = transform.Find("Icon").GetComponent<Image>();
         count = transform.Find("Count").GetComponent<TextMeshProUGUI>();
+        
+        
+
+        // 动态添加 EventTrigger 组件
+        AddEventTrigger();
+        
+        
     }
 
     public void SetUIWithMaterial(MaterialSlotInfo material)
     {
         icon.sprite = material.materialData.materialIcon;
         count.text = material.amount.ToString();
+        detailText = material.materialData.materialDesc;
     }
 
     public void SetUIWithTrap(TrapSlotInfo trap)
     {
         icon.sprite = trap.trapData.trapIcon;
         count.text = trap.amount.ToString();
+        detailText = trap.trapData.trapDesc;
     }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        timer = 0f; // 重置计时器
+        StartCoroutine(ShowDetailPanel()); // 开始计时
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopAllCoroutines(); // 停止所有协程
+        timer = 0f; // 重置计时器
+        Signals.Get<HideItemDetailSignal>().Dispatch();
+    }
+
+    private IEnumerator ShowDetailPanel()
+    {
+        while (true)
+        {
+            timer += Time.deltaTime;
+            if (timer >= delay)
+            {
+                Signals.Get<ShowItemDetailSignal>().Dispatch(this);
+                break;
+            }
+            yield return null;
+        }
+    }
+    private void AddEventTrigger()
+    {
+        // 获取或添加 EventTrigger 组件
+        EventTrigger trigger = gameObject.GetComponent<EventTrigger>() ?? gameObject.AddComponent<EventTrigger>();
+
+        // 添加 PointerEnter 事件
+        EventTrigger.Entry entryEnter = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        entryEnter.callback.AddListener((data) => { OnPointerEnter((PointerEventData)data); });
+        trigger.triggers.Add(entryEnter);
+
+        // 添加 PointerExit 事件
+        EventTrigger.Entry entryExit = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerExit
+        };
+        entryExit.callback.AddListener((data) => { OnPointerExit((PointerEventData)data); });
+        trigger.triggers.Add(entryExit);
+    }
+    
+    
 }
