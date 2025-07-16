d8ba47af74a78e05b07c6b0d9014ea5a4de44fb8
YuzeNeko <1992271758@qq.com>
2025-07-17 00:07:46 +0800

    dev:Const以及手持基类

---
+++ Assets/Scripts/SoLoader/EntityConst.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Entity的常量定义类 即工作单的名字
+/// </summary>
+public static class EntityConst
+{
+    //public const string Test = "222";
+}
