77bd248eec9348834214eb05bd774f9e59652d37
四不和声 <1223736337@qq.com>
2025-08-19 20:45:19 +0800

    dev:大桌子初步等

---
+++ Assets/Scripts/InteractObject/Item/Furniture/BigTable.cs
@@ -0,0 +1,16 @@
+using KidGame.Interface;
+using UnityEngine;
+
+namespace KidGame.Core
+{
+    public class BigTable : MapFurniture, IInteractive
+    {
+        public void InteractNegative(GameObject interactor) { }
+
+        public void InteractPositive(GameObject interactor)
+        {
+
+        }
+
+    }
+}
